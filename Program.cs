5af0d151d391e1d72c27a018ecaa4bc3ff4488c9
AlexFerJesus <alexfernandesdejesus.12@gmail.com>
2023-10-21 11:28:56 -0300

    Realização da tarefa e teste tambem. Tudo pronto para mandar

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("+-----Múltiplos----+\n");
+Console.WriteLine("+-Digite um numero-+");
+
+int nu1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Analisar se é múltiplo de:");
+
+int nu2 = Convert.ToInt32(Console.ReadLine());
+
+if (nu1 % nu2 == 0)
+{
+    Console.WriteLine($"{nu1} é multiplo de {nu2}");
+}
+else
+{
+     Console.WriteLine($"{nu1} não é multiplo de {nu2}");
+}
